b2402a3bf85ff4d8c0e1f6739326ae3521fdf601
AlexandrSMR77 <AlexandrSMR77@Gmail.com>
2021-06-06 17:06:35 +0400

    Старт

--- Main.py
+++ Main.py
@@ -1,1 +1,28 @@
-print('Да')
+def initial_filling(file):
+	file.close()
+	file = open('PrimeNumbers.txt', 'w+')
+	file.write('1\n2\n3\n5\n7\n')
+
+
+JUMP_ARRAY = [0, 6, 4, 2, 4, 2, 4, 6]
+
+primeNumbers = []
+
+fileNumbers = open('PrimeNumbers.txt', 'r+')
+for number in fileNumbers:
+	primeNumbers.append(int(number))
+
+if len(primeNumbers) < 5:
+	initial_filling(fileNumbers)
+	primeNumbers = [7]
+else:
+	primeNumbers.pop(0)
+	primeNumbers.pop(0)
+	primeNumbers.pop(0)
+	primeNumbers.pop(0)
+
+print(primeNumbers)
+
+fileNumbers.write(str(7))
+
+fileNumbers.close()
